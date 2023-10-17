632bd92135d6101519a82ad5c6c3c463b22cd377
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2023-10-17 13:38:59 -0400

    v0.4
    muhahahaha the Kings Gambit is BACKKK

--- gameProgramming/gaming_exercises/02_Collections/collections.py
+++ gameProgramming/gaming_exercises/02_Collections/collections.py
@@ -1,4 +1,4 @@
-# Collections 02 Johnson Traevon v0.3
+# Collections 02 Johnson Traevon v0.4
 
 """ 
 playerInventory = []
@@ -30,3 +30,32 @@ if key in doorKeys:
     print("You have the correct Key!\n")
 else:
     print(f"You don't have any of these keys\n {doorKeys}.\n") """
+
+weaponList = [
+    True, # Sword of Oblivion
+    False, # Spear of Recreation
+    True, # Magnus Carlsen Shirtless
+    True, # Traevon's Banana
+    False, # Nazi's Gernade
+    False, # Minecraft's Command Block
+    True, # Chess King
+]
+
+weaponNum = 0
+while weaponNum < len(weaponList):
+    if weaponNum == 0 and weaponList[0] == True: 
+        print("You are wielding a Destructours Sword.\n")
+    if weaponNum == 0 and weaponList[1] == True: 
+        print("You are wielding A Beautiful Spear.\n")
+    if weaponNum == 0 and weaponList[2] == True: 
+        print("You have something Special.\n")
+    if weaponNum == 0 and weaponList[3] == True: 
+        print("You are wielding something interesting...\n")
+    if weaponNum == 0 and weaponList[4] == True: 
+        print("Yo Watch out!\n")
+    if weaponNum == 0 and weaponList[5] == True: 
+        print("Whoa!!! Check your inventory dude!\n")
+    if weaponNum == 0 and weaponList[6] == True: 
+        print("You have played the Kings Gambit.\n")
+    weaponNum += 1
+    

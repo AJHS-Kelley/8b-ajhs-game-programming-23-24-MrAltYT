a6ed7a753b42d1f6dc2e32e6272b72f5fb73ed6a
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2023-10-17 13:03:38 -0400

    v0.2

--- gameProgramming/gaming_exercises/02_Collections/collections.py
+++ gameProgramming/gaming_exercises/02_Collections/collections.py
@@ -1,10 +1,17 @@
-# Collections 02 Johnson Traevon v0.1 
+# Collections 02 Johnson Traevon v0.2 
 
 
 playerInventory = []
 
 while len(playerInventory) < 10: 
     item = input("What Item do you want to add to your Inventory\n")
-    playerInventory.append(item)
+    playerInventory.append
 playerInventory.sort()
 print(playerInventory)
+
+while len(playerInventory) > 5:
+    item = input("What Item do you want to use from your Inventory\n")
+    playerInventory.remove(item)
+playerInventory.sort()
+print(playerInventory) 
+    

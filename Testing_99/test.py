80eb9145d83c9d5d0015fd221121b3a6b9af8757
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2023-10-23 13:58:09 -0400

    cloned

--- Testing_99/test.py
+++ Testing_99/test.py
@@ -22,32 +22,3 @@ print(testingMaterial)
 
 
 
-# Copy and Paste Below!!
-
-# if Difficulty == "Impossible" or Difficulty == "impossible":
-#             rangeMin = 0
-#             rangeMax = 1000
-#             numGuesses = 8
-#             print("You have chosen impossible!\n")
-#             print("You need to guess a number from 0 to 1000 and you have eight guesses.\n if you guess it right, you get a point\n if you can't guess it in eight guesses, the CPU gets a point\n")
-
-#         elif Difficulty == "Hard" or Difficulty == "hard":
-#             rangeMin = 0
-#             rangeMax = 375
-#             numGuesses = 6
-#             print("You have chosen Hard!\n")
-#             print("You need to guess a number from 0 to 375 and you have six guesses.\n if you guess it right, you get a point\n if you can't guess it in six guesses, the CPU gets a point\n")
-
-#         elif Difficulty == "Normal" or Difficulty == "normal":
-#             rangeMin = 0
-#             rangeMax = 75
-#             numGuesses = 4
-#             print("You have chosen Normal!\n")
-#             print(f"You need to guess a number from {rangeMin} to {rangeMax} and you have {numAttempts} guesses.\n if you guess it right, you get a point\n if you can't guess it in four guesses, the CPU gets a point\n")
-
-#         else:
-#             rangeMin = 0
-#             rangeMax = 25
-#             numGuesses = 3
-#             print("You have chosen Easy!\n")
-#             print(f"You need to guess a number from {rangeMin} to {rangeMax} and you have {numAttempts} guesses.\n if you guess it right, you get a point\n if you can't guess it in three guesses, the CPU gets a point\n")

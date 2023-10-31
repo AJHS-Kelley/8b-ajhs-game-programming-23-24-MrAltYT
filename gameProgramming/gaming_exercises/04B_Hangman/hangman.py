66c3aafac1292110796bbc797f4806aa608a0381
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2023-10-31 14:18:21 -0400

    v0.2
    GOD THAT WAS TRICKY

--- gameProgramming/gaming_exercises/04B_Hangman/hangman.py
+++ gameProgramming/gaming_exercises/04B_Hangman/hangman.py
@@ -1,1 +1,41 @@
-# 04B--Hangman By Johnson Traevon 10/31/23 v0.0
+# 04B--Hangman By Johnson Traevon 10/31/23 v0.2
+import random
+
+words = 'aqua scp batman reconstruction rehabilitation celestial brilliant segregational continentaldrift blunder chess inaccuracy Pseudopseudohypoparathyroidism Pneumonoultramicroscopicsilicovolcanoconiosis honorificabilitudinitatibus chargoggagoggmanchauggagoggchaubunagungamaugg radiation '.split()
+HANGMAN_BOARD = ['''
+    +---+             
+    O   |
+        |
+        |      
+    =======''','''
+    +---+
+    O   |
+    |   |
+        |
+    =======''','''
+    +---+
+    O   |
+   /|   |
+        |
+    =======''','''
+    +---+
+    O   |
+   /|\  |
+        |
+    =======''','''
+    +---+
+    O   |
+   /|\  |
+   /    |
+    =======''','''
+    +---+
+    O   |
+   /|\  |
+   / \  |
+    =======''']
+
+    
+i = 0
+while i < len(HANGMAN_BOARD):
+    print(HANGMAN_BOARD[i])
+    i += 1

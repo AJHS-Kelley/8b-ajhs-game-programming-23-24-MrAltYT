965045badbcaa2e149131b64a052d2b2614c706d
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2023-11-08 14:51:42 -0500

    no worries

--- Testing_99/test.py
+++ Testing_99/test.py
@@ -1,4 +1,5 @@
 #8B Traevon Johnson 2:18pm v0.0
+from random import randint
 print("Sup people welcome to my Test server!")
 testingMaterial = ["Scythe", "RoseScythe"]
 print(testingMaterial)
@@ -12,7 +13,11 @@ print(testingMaterial)
 
 
 
+i = ['a b c d e f g h i j'.split()]
 
+for i in range(10):
+    print((99, 1, 20))
+    print(randint(6, 11))
 
 
 

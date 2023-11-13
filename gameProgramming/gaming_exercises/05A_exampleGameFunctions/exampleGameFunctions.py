4bd1a2588abb18d55f93f40d5b726e382e10fcb6
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2023-11-13 14:50:05 -0500

    v0.1
    Before the game starts

--- gameProgramming/gaming_exercises/05A_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/gaming_exercises/05A_exampleGameFunctions/exampleGameFunctions.py
@@ -1,4 +1,4 @@
-# 05A--GameFunctions By Johnson Traevon 11/13/23 v0.0
+# 05A--PlatformerGameFunctions By Johnson Traevon 11/13/23 v0.1
 import random
 
 def functionOne():
@@ -12,3 +12,25 @@ def functionThree(param1 = "Default Value"):
 
 def functionFour(param1, param2, param3):
     pass
+ 
+characters = ["Goku Saitama Anya AiHoshino Kanada"] 
+
+
+print("Welcome to Anime Abologists!")
+characterChoice = input("Choose Your Character")
+
+
+def startGame(characterChoice):
+    if characterChoice == "Goku" or "goku":
+        print("You have chosen Goku!\n")
+    elif characterChoice == "Saitama" or "saitama":
+        print("You have chosen Saitama!\n")
+    elif characterChoice == "Anya" or "anya":
+        print("You have chosen Anya!\n")
+    elif characterChoice == "AiHoshino" or "aihoshino":
+        print("You have chosen Ai Hoshino!\n")
+    elif characterChoice == "Kanada" or "kanada":
+        print("You have chosen Kanada!\n")
+    return startGame
+
+startGame(characters)

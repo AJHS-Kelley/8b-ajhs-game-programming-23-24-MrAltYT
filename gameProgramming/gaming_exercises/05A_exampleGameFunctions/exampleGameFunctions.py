b45a4cb7b847bc8bf83645251910050f35d91268
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2023-11-15 14:26:04 -0500

    v0.2
    choose your character!

--- gameProgramming/gaming_exercises/05A_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/gaming_exercises/05A_exampleGameFunctions/exampleGameFunctions.py
@@ -1,36 +1,61 @@
-# 05A--PlatformerGameFunctions By Johnson Traevon 11/13/23 v0.1
+# 05A--PlatformerGameFunctions By Johnson Traevon 11/13/23 v0.2
 import random
 
-def functionOne():
-    pass
 
-def functionTwo(param1):
-    pass
 
-def functionThree(param1 = "Default Value"):
-    pass
-
-def functionFour(param1, param2, param3):
-    pass
  
-characters = ["Goku Saitama Anya AiHoshino Kanada"] 
+characters = ['goku', 'saitama', 'anya', 'aihoshino', 'kanada']
 
 
-print("Welcome to Anime Abologists!")
-characterChoice = input("Choose Your Character")
+
+print("Welcome to Anime Abologists!\n")
+
+print(f"Here are the character choices so far: {characters}")
+
+characterChoice = input("Choose Your Character\n").lower()
 
 
-def startGame(characterChoice):
-    if characterChoice == "Goku" or "goku":
+if characterChoice in characters:
+    if characterChoice == 'Goku' or characterChoice == "goku":
         print("You have chosen Goku!\n")
-    elif characterChoice == "Saitama" or "saitama":
-        print("You have chosen Saitama!\n")
-    elif characterChoice == "Anya" or "anya":
-        print("You have chosen Anya!\n")
-    elif characterChoice == "AiHoshino" or "aihoshino":
-        print("You have chosen Ai Hoshino!\n")
-    elif characterChoice == "Kanada" or "kanada":
-        print("You have chosen Kanada!\n")
-    return startGame
 
-startGame(characters)
+    elif characterChoice == "Saitama" or characterChoice == "saitama":
+        print("You have chosen Saitama!\n")
+
+    elif characterChoice == "Anya" or characterChoice == "anya":
+        print("You have chosen Anya!\n")
+
+    elif characterChoice == "AiHoshino" or characterChoice == "aihoshino":
+        print("You have chosen Ai Hoshino!\n")
+
+    elif characterChoice == "Kanada" or characterChoice == "kanada":
+        print("You have chosen Kanada!\n")
+
+else: print(f"Please choose a character from {characters}")
+
+mapChoice = input("Choose your map\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def functionOne():
+#     pass
+
+# def functionTwo(param1):
+#     pass
+
+# def functionThree(param1 = "Default Value"):
+#     pass
+
+# def functionFour(param1, param2, param3):
+#     pass

f177b13cea9c25057539f4610616fb4515e8f19b
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2023-12-12 14:15:00 -0500

    v0.3
    ALL I DO IS WIN WIN WIN NO MATTER WHAT

--- gameProgramming/gaming_exercises/06_OOP/oop.py
+++ gameProgramming/gaming_exercises/06_OOP/oop.py
@@ -1,4 +1,4 @@
-# Object-Oriented Programming, Johnson Traevon, v0.1
+# Object-Oriented Programming, Johnson Traevon, v0.3
 
 class Person: # Use PascalCase for ClassNames
     def __init__(self, name, age, weight):
@@ -7,12 +7,38 @@ class Person: # Use PascalCase for ClassNames
         self.weight = weight
 
     def __str__(self):
-        return f"Name: {self.name}\nShe is {self.age} years old.\nShe is also {self.weight} pounds in weight"
+        return f"Name: {self.name}\nShe is {self.age} years old.\nShe is also {self.weight} pounds in weight\n"
 
+    def classFunction(self):
+        print("This is an example class function\n")
+        print("It only works when called on an object of that class")
+        
+person1 = Person("Chika Fujiwara", 16, 142)
+person2 = Person("Kaguya Shinomiya", 15, 127)
+# print(person1)
+# print(person2)
 
-person1 = Person("Chika Fujiwara", 16, 132)
-print(person1)
+# print("                \a\n")
 
+# if person1.weight > person2.weight:
+#     print(f"{person1.name} weighs more than {person2.name}\n")
+# elif person1.weight == person2.weight:
+#     print(f"{person1.name} weighs the same as {person2.name}.\n")
+# else: 
+#     print(f"{person2.name} weighs more than {person1.name}.\n")
 
+# print("                     \n")
 
+# if person1.age > person2.age:
+#     print(f"{person1.name} is older than {person2.name}!\n")
+# elif person1.age == person2.age:
+#     print(f"{person1.name} and {person2.name} are the same age!\n")
+# else: 
+#     print(f"{person2.name} is older than {person1.name}\n")
 
+# person1.classFunction()
+
+# Changing Properties 
+print(person1.name)
+person1.name = "Chika Fujiwara"
+print(person1.name)

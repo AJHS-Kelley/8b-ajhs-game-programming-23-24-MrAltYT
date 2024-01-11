710d131be456d7c9b69b24ead8e8928ba4cd3200
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2024-01-11 14:23:29 -0500

    v0.2
    bases of life segregator constellation of modules

--- gameProgramming/gaming_exercises/Cohort_Lessons/07_DNAGame/dnagame.py
+++ gameProgramming/gaming_exercises/Cohort_Lessons/07_DNAGame/dnagame.py
@@ -1,7 +1,26 @@
-# DNA Replication Game, Johnson Traevon v0.1
+# DNA Replication Game, Johnson Traevon v0.2
 
 # Import Entire Modules -- Get the whole tool box
 import time, datetime
 
 # Import Specific Methods -- Get the specific tool
 from random import choice
+
+# Store the DNA Bases
+dnsBases = ["A", "T", "G", "C"]
+
+# Game Functions
+def gameIntro() -> None:
+    pass
+
+def genDNA() -> str:
+    basesGenerated = 0
+    basesRequested = int(input("How many bases do you want, Please enter a possitive number\n"))
+    dnaSequence = ""
+
+    while basesGenerated < basesRequested:
+        dnaSequence += choice(dnsBases)
+        basesGenerated += 1
+    return dnaSequence
+dna = genDNA()
+print(dna)

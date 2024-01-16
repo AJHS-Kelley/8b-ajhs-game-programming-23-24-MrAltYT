52a954498becee957db641be9684faf659ac54b8
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2024-01-16 14:07:18 -0500

    v0.4
    hoshi sama <3

--- gameProgramming/gaming_exercises/Cohort_Lessons/07_DNAGame/dnagame.py
+++ gameProgramming/gaming_exercises/Cohort_Lessons/07_DNAGame/dnagame.py
@@ -1,4 +1,4 @@
-# DNA Replication Game, Johnson Traevon v0.3
+# DNA Replication Game, Johnson Traevon v0.4
 
 # Import Entire Modules -- Get the whole tool box
 import time, datetime
@@ -7,7 +7,7 @@ import time, datetime
 from random import choice
 
 # Store the DNA Bases
-dnsBases = ["A", "T", "G", "C"]
+dnaBases = ["A", "T", "G", "C"]
 
 # Game Functions
 def gameIntro() -> None:
@@ -19,14 +19,12 @@ def genDNA() -> str:
     dnaSequence = ""
 
     while basesGenerated < basesRequested:
-        dnaSequence += choice(dnsBases)
+        dnaSequence += choice(dnaBases)
         basesGenerated += 1
     return dnaSequence
 
-dna = genDNA()
-print(dna)
 
-def genRNA(dnaSequence: str) -> tuple:
+def doTranscription(dnaSequence: str) -> tuple:
     print(f"The DNA Sequence is {dnaSequence}.\n")
     print("You will now generate the RNA sequence that would match\n")
     print("Please remember, in the RNA sequence U pairs with A from the DNA sequence")
@@ -36,10 +34,30 @@ def genRNA(dnaSequence: str) -> tuple:
     rnaTime = rnaStop - rnaStart
     return (rnaSequence, rnaTime)
 
-
     # Tuples are ORDERED -- you can reference itmes with the index
     # Tuples are UNCHANGEABLE -- you cannot add, modify, or delete after creating
     # Tuples can have duplicate values
+def verifySequence(dnaSequence: str, rnaSequence: str) -> bool:
+    isMatch = False
+    if len(dnaSequence) != len(rnaSequence):
+        print("Since the Sequence are not equal, they do not match.\n")
+        return isMatch
+    for dnaBase, rnaBase in zip(dnaSequence, rnaSequence):
+        if dnaBase == "A" and rnaBase == "U":
+            isMatch = True
+        elif dnaBase == "C" and rnaBase == "G":
+            isMatch = True
+        elif dnaBase == "G" and rnaBase == "C":
+            isMatch =  True
+        elif dnaBase == "T" and rnaBase == "A":
+            isMatch = True
+        else:
+            print("They do not match because no base is True.\n")
+    return isMatch
 dna = genDNA()
-rna = genRNA(dna)
-print(rna)
+rna = doTranscription(dna)
+print(verifySequence(dna, rna[0]))
+
+
+
+

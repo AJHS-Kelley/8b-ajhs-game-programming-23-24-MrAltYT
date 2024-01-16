a871ac02d64d4c58f6843ad34ffc9548e5b31cad
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2024-01-16 14:12:04 -0500

    YOUR WRONG KIDD

--- gameProgramming/gaming_exercises/Cohort_Lessons/07_DNAGame/dnagame.py
+++ gameProgramming/gaming_exercises/Cohort_Lessons/07_DNAGame/dnagame.py
@@ -23,13 +23,12 @@ def genDNA() -> str:
         basesGenerated += 1
     return dnaSequence
 
-
 def doTranscription(dnaSequence: str) -> tuple:
     print(f"The DNA Sequence is {dnaSequence}.\n")
     print("You will now generate the RNA sequence that would match\n")
     print("Please remember, in the RNA sequence U pairs with A from the DNA sequence")
     rnaStart = time.time() # time.time() returns the number of seconds since 00:00:00 UTC Jan, 01, 1970
-    rnaSequence = input("Please enter the matching RNA sequence.  Leave no spaces! Then press enter.\n")
+    rnaSequence = input("Please enter the matching RNA sequence.  Leave no spaces! Then press enter.\n").upper()
     rnaStop = time.time()
     rnaTime = rnaStop - rnaStart
     return (rnaSequence, rnaTime)

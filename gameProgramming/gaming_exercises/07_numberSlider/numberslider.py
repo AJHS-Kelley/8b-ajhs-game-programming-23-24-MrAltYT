423062239615227120d54f45f6e86515a9fc46fd
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2024-01-22 13:59:27 -0500

    v0.0
    no pygame L

--- gameProgramming/gaming_exercises/07_numberSlider/numberslider.py
+++ gameProgramming/gaming_exercises/07_numberSlider/numberslider.py
@@ -6,3 +6,12 @@ import sys, random, pygame
 from pygame.locals import *
 # Allows us to call features from pygame using just the function name instead of module.function()
 # Example: We can use draw() instead of pygame.draw()
+
+# Constants for game board
+BOARDWIDTH = 4 # COLUMNS
+BOARDHEIGT = 4 # ROWS
+TILESIZE = 80 # MEASURED IN PIXELS
+WINDOWWIDTH = 640 # MEASURED IN PIXELS
+WINDOWHEIGHT = 480 # MEASURED IN PIXELS
+FPS = 5 # This is a maximum value! Won't make a slow computer run faster
+BLANK = None

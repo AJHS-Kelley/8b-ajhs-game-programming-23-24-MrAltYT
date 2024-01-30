385aad99aa4486e9be4ff2046ccb2ad20984a010
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2024-01-30 14:36:38 -0500

    adding first survace

--- gameProgramming/gaming_exercises/08_ultimatePygame/projektm.py
+++ gameProgramming/gaming_exercises/08_ultimatePygame/projektm.py
@@ -1,18 +1,25 @@
-# Projekt M by Johnson Traevon 1:49pm 1/30/24 v0.1
+# Projekt M by Johnson Traevon 1:49pm 1/30/24 v0.1.5
 import pygame
 from sys import exit
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((1200,800))
+pygame.display.set_caption('A.R.G.U.S')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((600,400))
+test_surface.fill('purple')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(test_surface,(0,0))
     # Draw all our elements
     # Update everything
     pygame.display.update()
-
+    clock.tick(60)
 
 
 

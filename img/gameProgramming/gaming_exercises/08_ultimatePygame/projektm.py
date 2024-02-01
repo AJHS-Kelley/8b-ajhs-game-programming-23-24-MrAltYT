a1e3b8e7b926d3d5f026a896ede298ba5162825e
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2024-02-01 14:54:16 -0500

    had to reset some v0.2

--- img/gameProgramming/gaming_exercises/08_ultimatePygame/projektm.py
+++ img/gameProgramming/gaming_exercises/08_ultimatePygame/projektm.py
@@ -1,17 +1,25 @@
-# Projekt M by Johnson Traevon 1:49pm 1/30/24 v0.1
+# Projekt M by Johnson Traevon 2.53pm 2/1/24 v0.2
 import pygame
 from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('A.R.G.U.S')
+clock = pygame.time.Clock()
 
+sky_surface = pygame.image.load('img/blossom.jpg')
+ground_surface = pygame.image.load('img/ground.jpg')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+        screen.blit(sky_surface,(0,0))
+        screen.blit(ground_surface,(0,0))
     # Draw all our elements
     # Update everything
     pygame.display.update()
+    clock.tick(60)
 
 
 

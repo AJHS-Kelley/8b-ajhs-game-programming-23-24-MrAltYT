899e63c9057fe6b273acaf9aaaf0f9cdca61059e
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2024-02-01 14:45:54 -0500

    test

--- img/gameProgramming/gaming_exercises/08_ultimatePygame/projektm.py
+++ img/gameProgramming/gaming_exercises/08_ultimatePygame/projektm.py
@@ -1,28 +1,20 @@
-# Projekt M by Johnson Traevon 1:49pm 1/30/24 v0.1.5
+# Projekt M by Johnson Traevon 1:49pm 1/30/24 v0.1
 import pygame
 from sys import exit
 pygame.init()
-screen = pygame.display.set_mode((1200,800))
-pygame.display.set_caption('A.R.G.U.S')
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((800,400))
 
-test_surface = pygame.Surface((600,400))
-test_surface.fill('purple')
-
-# Draw all our elements
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
-    screen.blit(test_surface,(200,100))
-    
+    # Draw all our elements
     # Update everything
     pygame.display.update()
-    clock.tick(60)
 
-# STOPED AT 32:36-----
+
+
 
 
 

8bb3cb2432a6f5987095c3e587b6eb8d4f489533
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2024-02-05 21:38:10 -0500

    v0.4
    Added player and changed player x stars

--- img/gameProgramming/gaming_exercises/08_ultimatePygame/projektm.py
+++ img/gameProgramming/gaming_exercises/08_ultimatePygame/projektm.py
@@ -1,4 +1,4 @@
-# Projekt M by Johnson Traevon 2.53pm 2/1/24 v0.3.5
+# Projekt M by Johnson Traevon 2.53pm 2/1/24 v0.4
 import pygame
 from sys import exit
 
@@ -8,12 +8,15 @@ pygame.display.set_caption('A.R.G.U.S')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('img/Seagram_tfb 2.ttf', 33)
 
-sky_surface = pygame.image.load('img/blossom.jpg')
-ground_surface = pygame.image.load('img/ground.jpg')
+sky_surface = pygame.image.load('img/blossom.jpg').convert_alpha()
+ground_surface = pygame.image.load('img/ground.jpg').convert_alpha()
 test_surface = test_font.render('Welcome to Oshi No Ko!!', True, 'cyan')
 
-ai_surface = pygame.image.load('img/AI.gif')
-ai_x_pos = 400
+Stars = pygame.image.load('img/Star_Twins.gif').convert_alpha()
+Stars_x_pos = 800
+
+player_surf = pygame.image.load('img/AI.gif').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,250))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,10 +26,12 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,225))
     screen.blit(test_surface,(200,50))
-    ai_x_pos -= 3
-    if ai_x_pos <= -100:
-        ai_x_pos = 400
-    screen.blit(ai_surface,(ai_x_pos,75))
+    Stars_x_pos -= 3
+    if Stars_x_pos <= -300:
+        Stars_x_pos = 800
+    screen.blit(Stars,(Stars_x_pos,200))
+    screen.blit(player_surf,player_rect)
+    
 
     # Draw all our elements
     # Update everything

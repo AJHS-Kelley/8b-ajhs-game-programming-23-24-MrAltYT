7fdae5e4bd4423de7c60c4b53ac531abff072d15
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2024-02-13 14:41:40 -0500

    v0.5
    COLLISION

--- img/gameProgramming/gaming_exercises/08_ultimatePygame/projektm.py
+++ img/gameProgramming/gaming_exercises/08_ultimatePygame/projektm.py
@@ -1,4 +1,4 @@
-# Projekt M by Johnson Traevon 2.53pm 2/1/24 v0.4
+# Projekt M by Johnson Traevon 2.53pm 2/1/24 v0.5
 import pygame
 from sys import exit
 
@@ -12,26 +12,35 @@ sky_surface = pygame.image.load('img/blossom.jpg').convert_alpha()
 ground_surface = pygame.image.load('img/ground.jpg').convert_alpha()
 test_surface = test_font.render('Welcome to Oshi No Ko!!', True, 'cyan')
 
-Stars = pygame.image.load('img/Star_Twins.gif').convert_alpha()
-Stars_x_pos = 800
+Stars_surf = pygame.image.load('img/Star_Twins.gif').convert_alpha()
+Stars_rect = Stars_surf.get_rect(bottomright = (600,300))
 
 player_surf = pygame.image.load('img/AI.gif').convert_alpha()
-player_rect = player_surf.get_rect(midbottom = (80,250))
+player_rect = player_surf.get_rect(midbottom = (80,300))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEMOTION:
+            if player_rect.collidepoint(event.pos): 
+                print('collision')
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,225))
     screen.blit(test_surface,(200,50))
-    Stars_x_pos -= 3
-    if Stars_x_pos <= -300:
-        Stars_x_pos = 800
-    screen.blit(Stars,(Stars_x_pos,200))
+    Stars_rect.x -= 3
+    if Stars_rect.right <= -300:
+        Stars_rect.left = 800
+    screen.blit(Stars_surf, Stars_rect)
     screen.blit(player_surf,player_rect)
     
+    # if player_rect.colliderect(Stars_rect):
+    #     print('collision')
+    mouse_pos = pygame.mouse.get_pos()
+    if player_rect.collidepoint((mouse_pos)):
+        print(pygame.mouse.get_pressed())
 
     # Draw all our elements
     # Update everything

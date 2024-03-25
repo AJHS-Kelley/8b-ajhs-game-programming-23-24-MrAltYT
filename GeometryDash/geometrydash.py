4a052abc75bfa14af52204cecaf608682d1a05de
MrAltYT <111774198+MrAltYT@users.noreply.github.com>
2024-03-25 19:29:38 -0400

    v0.4
    Adding background images for intro

--- GeometryDash/geometrydash.py
+++ GeometryDash/geometrydash.py
@@ -1,23 +1,33 @@
-# 8B Geometry Dash Sponsored by RobTop, Created by Johnson Traveon and Christain Ortiz v0.2
+# 8B Geometry Dash Sponsored by RobTop, Created by Johnson Traveon and Christain Ortiz v0.4
 import pygame
 from sys import exit
 import random
+import gd
 
 pygame.init()
 screen = pygame.display.set_mode((1152,648))
 pygame.display.set_caption('Geometry Dash')
 clock = pygame.time.Clock()
-geometrydash_font = pygame.font.Font('oxygene-1/OXYGENE1.ttf', 40)
-geometry_surface = geometrydash_font.render('geometry dash', False, 'Green')
-geometry_rect = geometry_surface.get_rect(center = (375,150))
+
+geometry_bg = pygame.image.load('img/Gd background.jpg').convert_alpha()
+
+geometrydash_font = pygame.font.Font('img/Seagram.ttf', 80)
+geometry_surface = geometrydash_font.render('geometry dash', False, 'Green').convert_alpha()
+geometry_rect = geometry_surface.get_rect(center = (400,150))
+
+geometry_pb = pygame.image.load('img/Gd play.jpg').convert_alpha()
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(geometry_surface,(375,150))
-    pygame.draw.rect(geometry_surface, 'White',geometry_rect)
+    screen.blit(geometry_bg,(0,0))
+    screen.blit(geometry_surface,(350,65))
+    screen.blit(geometry_pb, (445,200))
+    pygame.draw.rect(geometry_surface,'Black',geometry_rect, 700)
+    
     
     pygame.display.update()
     clock.tick(60)

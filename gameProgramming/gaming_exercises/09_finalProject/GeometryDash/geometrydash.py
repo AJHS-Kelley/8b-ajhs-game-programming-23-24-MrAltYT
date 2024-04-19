96f6eb1184aba59a6ca159340af30293bbea2b70
MrAltYT <s20100391@students.duvalschools.org>
2024-04-19 14:54:19 -0400

    Update geometrydash.py

--- gameProgramming/gaming_exercises/09_finalProject/GeometryDash/geometrydash.py
+++ gameProgramming/gaming_exercises/09_finalProject/GeometryDash/geometrydash.py
@@ -43,8 +43,8 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
-
+screen2 = '' #LEVEL PICKER
+screen3 = "" #LEVEL
 GRAVITY = Vector2(0, 0.86)
 
 # BINTRICALIZATIONALISM
